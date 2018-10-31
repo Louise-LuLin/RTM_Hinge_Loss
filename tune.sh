6324ffe214887aaf33325bd54f27148e2da90ac4
Lu Lin <lulin199209@gmail.com>
2018-10-30 22:25:16 -0400

    use c++ style bash

--- tune.sh
+++ tune.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 read -p "Enter source, mode:" source mode
 echo
-for k in {5..50..5}
+for (( k=5; k<=50; k+5 ))
 do
     echo "$k"
 #	nohup ./run -prefix ~/lab/dataset -source $source -set byUser_20k_review -crossV 5 -nuOfTopics $k -emIter 50 -topicmodel RTM -mode $mode> ./output/"$mode"_"$source"_RTM_"$k".output 2>&1 &
