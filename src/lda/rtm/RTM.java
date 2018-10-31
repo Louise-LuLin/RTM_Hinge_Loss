3bae03d06293ce73c8c71a33893ab9222a670120
Lu Lin <lulin199209@gmail.com>
2018-10-31 10:54:26 -0400

    only output userembed when CVlink

--- src/lda/rtm/RTM.java
+++ src/lda/rtm/RTM.java
@@ -461,8 +461,8 @@ public class RTM extends LDA
 
             String modelFileName = String.format("%s/%d/%s_RTM_model_%d.txt",
                     outputFolder, i, param.m_mode, param.m_number_of_topics);
-            String userEmbedFileName = String.format("%s/%d/%s_RTM_userEmbed_%d.txt",
-                    outputFolder, i, param.m_mode, param.m_number_of_topics);
+            String userEmbedFileName = String.format("%s/%d/RTM_userEmbed_%d.txt",
+                    outputFolder, i, param.m_number_of_topics);
             (new File(userEmbedFileName)).getParentFile().mkdirs();
 
             RTM RTMTrain = new RTM(parameters);
