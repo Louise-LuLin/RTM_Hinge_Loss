83d0e7af2759b9195c4a78b4f508f049ad9ea2f2
Lu Lin <lulin199209@gmail.com>
2018-10-30 22:15:56 -0400

    bash

---
+++ tune.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+read -p "Enter source, mode:" source mode
+echo
+for k in {5..50..5}
+do
+	nohup ./run -prefix ~/lab/dataset -source $source -set byUser_20k_review -crossV 5 -nuOfTopics $k -emIter 50 -topicmodel RTM -mode $mode> ./output/"$mode"_"$source"_RTM_"$k".output 2>&1 &
+done
