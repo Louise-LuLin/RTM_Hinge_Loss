807e99cf8bf4430affa131a8e26657348045652c
Lu Lin <lulin199209@gmail.com>
2018-10-31 18:32:02 -0400

    add topic control

--- tune.sh
+++ tune.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-read -p "Enter source, mode, coldstart: " source mode cold
+read -p "Enter source, mode, coldstart, minTopic, maxTopic: " source mode cold min max
 echo
-for k in {5..50..5}
+for (( k = $min; k <= $max; k = k + 5 ))
 do
 	nohup ./run -prefix ~/lab/dataset -source $source -set byUser_20k_review -crossV 5 -nuOfTopics $k -emIter 50 -topicmodel RTM -mode $mode -flagColdstart $cold > ./output/"$mode"_"$cold"_"$source"_RTM_"$k".output 2>&1 &
 done
