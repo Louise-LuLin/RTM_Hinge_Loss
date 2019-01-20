67c26fdc949d57ad0c64a3ecb46095affda34bed
Louise-LuLin <lulin199209@gmail.com>
2019-01-20 18:07:30 +0000

    add tune.sh

--- tune.sh
+++ tune.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-read -p "Enter source, mode, coldstart, minTopic, maxTopic: " source mode cold min max
+read -p "Enter source, model, mode, coldstart, tune, minTopic, maxTopic: " source model mode cold tune min max
 echo
-for (( k = $min; k <= $max; k = k + 5 ))
+for (( k = $min; k <= $max; k = k + 10 ))
 do
-	nohup ./run -prefix ~/lab/dataset -source $source -set byUser_20k_review -crossV 5 -nuOfTopics $k -emIter 50 -topicmodel RTM -mode $mode -flagColdstart $cold > ./output/"$mode"_"$cold"_"$source"_RTM_"$k".output 2>&1 &
+	nohup ./run -flagTune $tune -varMaxIter 3 -prefix ~/lab/dataset -source $source -set byUser_20k_review -crossV 5 -nuOfTopics $k -emIter 50 -topicmodel $model -mode $mode -flagColdstart $cold > ./output/"$mode"_"$cold"_"$source"_RTM_"$model"_"$tune"_"$k".output 2>&1 &
 done
